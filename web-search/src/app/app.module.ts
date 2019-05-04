b6095314ab4d008028935493d92fb3d411940ca8
Hollie Wilson <holliebrooke97@yahoo.com>
2019-05-04 17:09:27 -0500

    instantsearch

--- web-search/src/app/app.module.ts
+++ web-search/src/app/app.module.ts
@@ -1,15 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgAisModule } from 'angular-instantsearch';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [NgAisModule.forRoot(), BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
