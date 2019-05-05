d554cc5e0edf85e7b03926f6a675b247de467f6c
Hollie Wilson <holliebrooke97@yahoo.com>
2019-05-04 19:37:32 -0500

    fix

--- web-search/README.md
+++ web-search/README.md
@@ -1,6 +1,6 @@
 # WebSearch
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.8.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.2.9.
 
 ## Development server
 
--- web-search/src/app/app.module.ts
+++ web-search/src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NgAisModule } from 'angular-instantsearch';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [NgAisModule.forRoot(), BrowserModule],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
